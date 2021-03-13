e6c6f16e918faf7dcd44828fd5883fc624ba86de
manandearth <adamgefen@posteo.net>
2021-03-13 10:10:45 +0100

    Add dynamic grid function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import * as React from "react";
 import Matrix from "./matrix.jsx";
 
 function App() {
+  const [level, setLevel] = React.useState(1);
+
   return (
     <div className="App">
       <h1>bird-game</h1>
-      <Matrix />
+      <h2>choose you level</h2>
+      <input onChange={e => setLevel(e.target.value)} />
+      <h2>{`level - ${level}`}</h2>
+      <Matrix level={level} />
     </div>
   );
 }
--- src/matrix.jsx
+++ src/matrix.jsx
@@ -1,26 +1,42 @@
 import * as React from "react";
 
-function Matrix() {
+function Matrix(props) {
+  const { level } = props;
+  const [matrix, setMatrix] = React.useState([]);
+
+  React.useEffect(() => {
+    let grid = [];
+    for (var i = 0; i < level; i++) {
+      grid[i] = [];
+      for (var j = 0; j < level; j++) {
+        grid[i][j] = undefined;
+      }
+      console.log(typeof level);
+    }
+
+    setMatrix(grid);
+  }, [level]);
+
   const fixedMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
 
   const [cardsFlipped, setCardsFlipped] = React.useState([]);
 
-  const handleClick = value => {
-    setCardsFlipped([...cardsFlipped, value]);
-    console.log(`I told you that I am ${value}`);
+  const handleClick = (row, column) => {
+    setCardsFlipped([...cardsFlipped, [row, column]]);
+    console.log(`I told you that I am ${row}-${column}`);
   };
 
   return (
     <div>
-      {fixedMatrix.map((row, index) => {
+      {matrix.map((row, rowIndex) => {
         return (
-          <div key={`row-${index}`}>
-            {row.map((card, index) => {
+          <div key={`row-${rowIndex}`}>
+            {row.map((column, columnIndex) => {
               return (
                 <button
-                  key={`button-${index}`}
-                  onClick={() => handleClick(card)}
-                >{`Hi I am ${card}`}</button>
+                  key={`button-${rowIndex}-${columnIndex}`}
+                  onClick={() => handleClick(rowIndex, columnIndex)}
+                >{`Hi I am ${rowIndex}-${columnIndex}`}</button>
               );
             })}
           </div>
